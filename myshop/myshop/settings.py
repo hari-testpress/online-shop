6024f9aa51fadab039acb84b222aca55fce14c77
hari-testpress <hari@testpress.in>
2021-09-19 16:43:52 +0530

    Create Cart class to manage cart session

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -129,5 +129,7 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CART_SESSION_ID = "cart"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
