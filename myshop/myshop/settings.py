f710b345407501a139c7957c64ad3aea3b472db1
hari-testpress <hari@testpress.in>
2021-09-19 09:07:06 +0530

    fix the bug in the installed apps settings

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "myshop.apps.ShopConfig",
+    "shop.apps.ShopConfig",
 ]
 
 MIDDLEWARE = [
