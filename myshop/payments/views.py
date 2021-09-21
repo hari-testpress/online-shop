a50c080b5a9dd4db48c6c5a275844bac59883e96
hari-testpress <hari@testpress.in>
2021-09-21 11:28:44 +0530

    Support payment gateway

--- myshop/payments/views.py
+++ myshop/payments/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+import braintree
+from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
+from orders.models import Order
 
-# Create your views here.
+
+# instantiate Braintree payment gateway
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+
+
+def payment_process(request):
+    order_id = request.session.get("order_id")
+    order = get_object_or_404(Order, id=order_id)
+    total_cost = order.get_total_cost()
+
+    if request.method == "POST":
+        nonce = request.POST.get("payment_method_nonce", None)
+        result = gateway.transaction.sale(
+            {
+                "amount": f"{total_cost:.2f}",
+                "payment_method_nonce": nonce,
+                "options": {"submit_for_settlement": False},
+            }
+        )
+        if result.is_success:
+            order.paid = True
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect("payment:done")
+        else:
+            print(dir(result.errors.errors))
+            return redirect("payment:canceled")
+    else:
+        # generate token
+        client_token = gateway.client_token.generate()
+        return render(
+            request,
+            "payments/process.html",
+            {"order": order, "client_token": client_token},
+        )
+
+
+def payment_done(request):
+    return render(request, "payments/done.html")
+
+
+def payment_canceled(request):
+    return render(request, "payments/canceled.html")
