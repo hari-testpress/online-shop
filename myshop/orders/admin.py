2b606be686a509bd6c47efc2d7b805508283e40c
hari-testpress <hari@testpress.in>
2021-09-22 11:52:09 +0530

    Add support to generate the invoice for an order in the PDF format

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -44,6 +44,14 @@ def export_to_csv(modeladmin, request, queryset):
 export_to_csv.short_description = "Export to CSV"
 
 
+def order_pdf(obj):
+    url = reverse("orders:admin_order_pdf", args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+
+
+order_pdf.short_description = "Invoice"
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -58,6 +66,7 @@ class OrderAdmin(admin.ModelAdmin):
         "created",
         "updated",
         order_detail,
+        order_pdf,
     ]
     list_filter = ["paid", "created", "updated"]
     actions = [export_to_csv]
